0ef6104aff4dc154e2de671a5995f137db332022
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-02-12 12:42:17 -0600

    Add fahrenheit->celsius function with associated tests in introductory namespace

--- 1. Introduction/introductory.clj
+++ 1. Introduction/introductory.clj
@@ -8,12 +8,23 @@
   [gibibytes]
   (* gibibytes 1024 1024 1024))
 
-(gibibytes->bytes 5)
+; Problem 2
+(defn fahrenheit->celsius
+  "Convert f degrees Fahrenheit to degrees Celsius."
+  [f]
+  (/ (* 5.0
+        (- f 32.0))
+     9.0))
 
 (deftest test-gibibytes->bytes
   (is (= 0 (gibibytes->bytes 0)))
   (is (= 1073741824 (gibibytes->bytes 1)))
   (is (= 5368709120 (gibibytes->bytes 5)))
   (is (= 26415122612224 (gibibytes->bytes 24601))))
+
+(deftest test-fahrenheit->celsius
+  (is (= 100.0 (fahrenheit->celsius 212.0)))
+  (is (= 0.0 (fahrenheit->celsius 32.0)))
+  (is (= -40.0 (fahrenheit->celsius -40.0))))
 
 (run-tests)
