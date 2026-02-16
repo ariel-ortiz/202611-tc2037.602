bd1ae00a9f256230816fe19143291e9a299757c5
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-02-16 12:53:55 -0600

    Add `roots` function and associated tests in introductory namespace

--- 1. Introduction/introductory.clj
+++ 1. Introduction/introductory.clj
@@ -16,7 +16,7 @@
         (- f 32.0))
      9.0))
 
-; Probleam 3
+; Problem 3
 (defn sign
   "Determines if n is positive (1), negative (-1) or zero (0)."
   [n]
@@ -24,6 +24,20 @@
     (neg? n) -1
     (pos? n) 1
     :else 0))
+
+; Problem 4
+(defn roots
+  "Computes the two roots to solve:
+        ax^2 + bx + c = 0
+  "
+  [a b c]
+  (let [d (- b)
+        e (sqrt (- (* b b)
+                   (* 4 a c)))
+        f (* 2 a)
+        x1 (/ (+ d e) f)
+        x2 (/ (- d e) f)]
+    [x1 x2]))
 
 (deftest test-gibibytes->bytes
   (is (= 0 (gibibytes->bytes 0)))
@@ -40,5 +54,10 @@
   (is (= -1 (sign -5)))
   (is (= 1 (sign 10)))
   (is (= 0 (sign 0))))
+
+(deftest test-roots
+  (is (= [-1 -1] (roots 2 4 2)))
+  (is (= [0 0] (roots 1 0 0)))
+  (is (= [-1/4 -1] (roots 4 5 1))))
 
 (run-tests)
