19353b9cbd114a69cf45288277b5915b9c682b6e
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-02-19 12:59:31 -0600

    Refactor and extend factorial functions: rename `!` to `fact-v1`, add iterative `fact-v2` and reduce-based `fact-v3` implementations, and update testing examples.

--- 2. Repetitions/simple_examples.clj
+++ 2. Repetitions/simple_examples.clj
@@ -1,18 +1,32 @@
 (ns simple-examples)
 
-(defn !
+(defn fact-v1
   [n]
   (if (zero? n)
     1
-    (*' n (! (dec n)))))
+    (*' n (fact-v1 (dec n)))))
 
-(! 0)
-(! 2)
-(! 3)
-(! 4)
-(! 5)
-(! 20)
-(! 30)
-(! 100)
-(! 1000)
-; (! 10000)
+(defn fact-v2
+  [n]
+  (loop [i 1
+         result 1]
+    (if (> i n)
+      result
+      (recur (inc i)
+             (*' result i)))))
+
+(defn fact-v3
+  [n]
+  (reduce *' (range 1 (inc n))))
+
+(fact-v1 1000)
+(fact-v2 0)
+(fact-v2 1)
+(fact-v2 5)
+(fact-v2 1000)
+(fact-v2 10000)
+(fact-v3 0)
+(fact-v3 1)
+(fact-v3 5)
+(fact-v3 1000)
+(fact-v3 10000)
