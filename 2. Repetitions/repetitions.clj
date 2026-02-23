269a491e680a4a95b13abf664879f2bb33598e91
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-02-23 11:41:15 -0600

    Add `positives` function with tests in `repetitions` namespace

--- 2. Repetitions/repetitions.clj
+++ 2. Repetitions/repetitions.clj
@@ -29,11 +29,22 @@
   [s]
   (map list s))
 
+; Problem 2
+(defn positives
+  [s]
+  (filter pos? s))
+
 (deftest test-enlist
   (is (= () (enlist ())))
   (is (= '((a) (b) (c)) (enlist '(a b c))))
   (is (= '((1) (2) (3) (4)) (enlist [1 2 3 4])))
   (is (= '(((1 2 3)) (4) ((5)) (7) (8))
          (enlist '((1 2 3) 4 (5) 7 8)))))
+
+(deftest test-positives
+  (is (= () (positives ())))
+  (is (= () (positives [-4 -1 -10 -13 -5])))
+  (is (= [3 6] (positives [-4 3 -1 -10 -13 6 -5])))
+  (is (= [4 3 1 10 13 6 5] (positives [4 3 1 10 13 6 5]))))
 
 (run-tests)
