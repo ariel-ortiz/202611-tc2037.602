a69a0d3604cd107d64b44f9ba3dd0e12ab1bb4a6
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-02-23 11:37:41 -0600

    Add `add-squares` function with tests in `repetitions` namespace

--- 2. Repetitions/repetitions.clj
+++ 2. Repetitions/repetitions.clj
@@ -34,6 +34,11 @@
   [s]
   (filter pos? s))
 
+; Problem 3
+(defn add-squares
+  [s]
+  (reduce + (map sqr s)))
+
 (deftest test-enlist
   (is (= () (enlist ())))
   (is (= '((a) (b) (c)) (enlist '(a b c))))
@@ -42,9 +47,14 @@
          (enlist '((1 2 3) 4 (5) 7 8)))))
 
 (deftest test-positives
-  (is (= () (positives ())))
   (is (= () (positives [-4 -1 -10 -13 -5])))
   (is (= [3 6] (positives [-4 3 -1 -10 -13 6 -5])))
   (is (= [4 3 1 10 13 6 5] (positives [4 3 1 10 13 6 5]))))
+
+(deftest test-add-squares
+  (is (= 0 (add-squares [])))
+  (is (= 25 (add-squares [5])))
+  (is (= 30 (add-squares [2 4 1 3])))
+  (is (= 385 (add-squares [1 2 3 4 5 6 7 8 9 10]))))
 
 (run-tests)
