7718b68da775f3fcf4de7fc4d5ca3d539ddc3fa5
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-05-21 11:43:53 -0600

    Add palindrome parser and corresponding tests using Instaparse

--- 7. Context-Free Grammars/grammar.clj
+++ 7. Context-Free Grammars/grammar.clj
@@ -6,6 +6,7 @@
 (defn fails? [r] (instance? Failure r))
 (defn succeeds? [r] (not (fails? r)))
 
+;;; Problem 1
 (def start-and-end (parser "
 
   S = '#'
@@ -32,5 +33,34 @@
   (is (fails? (start-and-end "###$$#$#$$$#$$$####$")))
   (is (fails? (start-and-end "$#$#$#$$$$#$$$#$$$$#$$#")))
   (is (fails? (start-and-end "#######################$"))))
+
+;;; Problem 2
+(def palindrome (parser "
+
+  P = epsilon
+    | '0'
+    | '1'
+    | '0' P '0'
+    | '1' P '1'
+
+"))
+
+(deftest test-palindrome
+  (is (succeeds? (palindrome "")))
+  (is (succeeds? (palindrome "0")))
+  (is (succeeds? (palindrome "1")))
+  (is (succeeds? (palindrome "11")))
+  (is (succeeds? (palindrome "00")))
+  (is (succeeds? (palindrome "010")))
+  (is (succeeds? (palindrome "1111111")))
+  (is (succeeds? (palindrome "000010000")))
+  (is (succeeds? (palindrome "01001110101110010")))
+  (is (fails? (palindrome "01")))
+  (is (fails? (palindrome "10")))
+  (is (fails? (palindrome "1010")))
+  (is (fails? (palindrome "10000000")))
+  (is (fails? (palindrome "00010001")))
+  (is (fails? (palindrome "1010011010")))
+  (is (fails? (palindrome "111111111111111111110"))))
 
 (run-tests)
